65affbd42ca38def8e6046c2a9e18ef5ed23fc88
Raja <rajarevaneri@gmail.com>
2024-06-05 20:22:08 +0700

    Update supabase.js

--- src/database/supabase.js
+++ src/database/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://xbvjjhdrmvgyecolvwwu.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhidmpqaGRybXZneWVjb2x2d3d1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU0NDU0MDQsImV4cCI6MjAzMTAyMTQwNH0.twmkYN8CMOFqnsd6jER5RV8ixp6_2JuCWbifGS_h0MM";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = "examplekey";
+const supabaseKey = "examplekey"
+  const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
